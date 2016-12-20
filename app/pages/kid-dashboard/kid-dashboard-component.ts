0b194056a7db9cdda5956c8cd50283bfa9fc4180
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-20 20:09:06 +0530

    UI: Kid dashboard Layout setup

--- app/pages/kid-dashboard/kid-dashboard-component.ts
+++ app/pages/kid-dashboard/kid-dashboard-component.ts
@@ -1,24 +1,47 @@
 import {Component, ViewChild, ElementRef, ChangeDetectorRef, OnInit} from "@angular/core";
-import {DrawerPage} from "../drawer.page";
+import { Page } from "ui/page";
 
-import { ListView } from 'ui/list-view';
-import { TextView } from 'ui/text-view';
-import { GridLayout } from "ui/layouts/grid-layout";
+import {CalendarService,  Message} from "../../shared/calendar.service";
 
+export class DataItem {
+    constructor(public itemDesc: string) {}
+}
 
 @Component({
     selector: 'my-app',
     styleUrls: ['pages/kid-dashboard/kid-dashboard.css'],
     templateUrl: 'pages/kid-dashboard/kid-dashboard.html',
-    providers: []
+    providers: [CalendarService]
 })
-export class KidDashboardComponent extends DrawerPage implements OnInit{
+export class KidDashboardComponent implements OnInit{
+    public other = {};
+    public items: Array<DataItem>;
+    public tags: Array<DataItem>;
+    public messages: Array<Message>;
 
-    constructor(private changeDetectorRef: ChangeDetectorRef) {
-        super(changeDetectorRef);
+    constructor(private calendarService: CalendarService,
+                private page: Page, private changeDetectorRef: ChangeDetectorRef) {
+        //super(changeDetectorRef);
+
+        const calendar = calendarService.getCalendar();
+        this.messages = calendar.messages;
+
+        this.other = {
+            name: 'Jannie Deo',
+            pictureUrl: 'https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100',
+            coverUrl: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=500%C3%97500&w=500&h=500',
+        };
+
+        this.items = new Array<DataItem>();
+        this.tags = new Array<DataItem>();
+        for (let i = 0; i < 5; i++) {
+            this.items.push(new DataItem("item " + i));
+            this.tags.push(new DataItem("tag " + i))
+        }
     }
 
     ngOnInit() {
+        //this.page.actionBarHidden = true;
 
     }
 
