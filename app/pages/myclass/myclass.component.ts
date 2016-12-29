a25fd01772130cf667d465043b2c8bd2b83b283b
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-29 11:49:03 +0530

    Kid singn-in/sign-out

--- app/pages/myclass/myclass.component.ts
+++ app/pages/myclass/myclass.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, ElementRef, ChangeDetectorRef, OnInit} from "@angular/core";
 import {DrawerPage} from "../drawer.page";
 import {MyClassService, ManagedKid, Room} from "../../shared/myclass.service";
+import { KidSignInOutService } from "../../shared/kid-signinout.service";
 
 import { ListView } from 'ui/list-view';
 import { TextView } from 'ui/text-view';
@@ -15,13 +16,14 @@ import { DatePipe } from '@angular/common';
     selector: 'my-app',
     styleUrls: ['pages/myclass/myclass.css'],
     templateUrl: 'pages/myclass/myclass.component.html',
-    providers: [MyClassService]
+    providers: [ MyClassService, KidSignInOutService ]
 })
 export class MyClassComponent extends DrawerPage implements OnInit{
     public managed_kids: Array<ManagedKid>;
     public room: String;
 
     constructor(private myClassService: MyClassService,
+                private kidSignInOutService: KidSignInOutService,
                 private changeDetectorRef: ChangeDetectorRef,
                 private datePipe: DatePipe) {
         super(changeDetectorRef);
@@ -32,9 +34,9 @@ export class MyClassComponent extends DrawerPage implements OnInit{
         this.myClassService.getManagedKids()
             .subscribe(
                 (result) => {
-                    console.log("MyClass resp: "+JSON.stringify(result))
+                    console.log("MyClass resp: "+JSON.stringify(result));
                     var body = result.body;
-                    this.managed_kids = body.managed_kids
+                    this.managed_kids = body.managed_kids;
                     this.room = body.session_name
                 },
                 (error) => {
@@ -50,20 +52,31 @@ export class MyClassComponent extends DrawerPage implements OnInit{
         console.log("Object that triggered the event: " + args.object);
         console.log("View that triggered the event: " + args.view);
         console.log("Event name: " + args.eventName);*/
-        var kidName = kid.fname + ' '+ kid.lname;
-        kidName = "'"+kidName+"'";
+        //var kidName = kid.fname + ' '+ kid.lname;
+        //kidName = "'"+kidName+"'";
 
         dialogs.action({
             message: "Your message",
             cancelButtonText: "Cancel",
-            actions: ["Sign-in", "Sign-out","Inform Parent"]
+            actions: ["Sign-in/Sign-out","Inform Parent"]
         }).then(result => {
-            if(result === 'Sign-in'){
-                var time = this.datePipe.transform(new Date(), 'HH:MM a');
-                alert(kidName + " signed in successfully at " + time)
-            }else if(result === 'Sign-out'){
-                var time = this.datePipe.transform(new Date(), 'HH:MM a');
-                alert(kidName + " signed out successfully at " + time)
+            if(result === 'Sign-in/Sign-out'){
+                //var time = this.datePipe.transform(new Date(), 'HH:MM a');
+                //alert(kidName + " signed in successfully at " + time)
+                this.kidSignInOutService.signInOrSingOut(kid.kid_klid)
+                    .subscribe(
+                        (result) => {
+                            console.log("Sign-in/Sign-out resp: "+JSON.stringify(result));
+                            var body = result.body;
+                            alert(body.text)
+                        },
+                        (error) => {
+                            console.log('Error in Sing-in/Sign-out: '+ JSON.stringify(error));
+                            alert(error._body.message || 'internal server error')
+                        }
+                    );
+
+
             }else if(result === "Inform Parent") {
 
                 dialogs.prompt({
---
+++ app/shared/kid-signinout.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from "@angular/http";
+import { Observable } from "rxjs/Rx";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
+import { Config } from "../shared/config";
+import { TokenService } from "../shared/token.service";
+import { TeacherInfo } from "../providers/data/teacher_info";
+
+@Injectable()
+export class KidSignInOutService {
+    constructor(private http: Http) {
+
+    }
+
+    signInOrSingOut(kid_klid) {
+        var room = TeacherInfo.parsedDetails.rooms[0];
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        let data = JSON.stringify({
+            access_token: TokenService.accessToken,
+            auth_token: TokenService.authToken,
+            command: "signin_signout",
+            body: {
+                session_id: room.session_id,
+                season_id: room.season_id,
+                kid_klid: kid_klid
+            }
+        });
+
+        return this.http.post(
+            Config.apiUrl + "attendances", data, {
+                headers: headers
+            }
+        ).map((res:Response) => res.json())
+            .catch(this.handleErrors);
+    }
+
+    handleErrors(error: any)  {
+        console.error('An error occurred', error); // for demo purposes only
+        return Observable.throw(error);
+    }
+}
