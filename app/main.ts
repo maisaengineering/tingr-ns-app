700104ecfbe7cddcebd0d3eaba376c163afd9866
Upender Devulapally <upender.devulapally@gmail.com>
2017-01-02 12:35:04 +0530

    production mode enabled

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,11 @@ import {platformNativeScriptDynamic} from "nativescript-angular/platform";
 import {AppModule} from "./app.module";
 import {StatusBar} from "./utils/native";
 
+// enable production mode...
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+
+
 StatusBar.setColor('#388e3c');
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
