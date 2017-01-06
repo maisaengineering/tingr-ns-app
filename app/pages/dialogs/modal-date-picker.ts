2c5705c4fe5c19f8ad6ea826ab895f1f23577680
Upender Devulapally <upender.devulapally@gmail.com>
2017-01-06 13:38:00 +0530

    v1.0: datepicker ui updated

--- app/pages/dialogs/modal-date-picker.ts
+++ app/pages/dialogs/modal-date-picker.ts
@@ -3,34 +3,43 @@ import {ModalDialogParams} from "nativescript-angular/directives/dialogs";
 import {ModalDialogService, ModalDialogOptions, ModalDialogHost} from "nativescript-angular/modal-dialog";
 import {DatePicker} from "ui/date-picker";
 import {Page} from "ui/page";
-import { Router, NavigationExtras } from "@angular/router";
+import {Router, NavigationExtras} from "@angular/router";
 
 @Component({
     selector: 'modal-content',
     template: ` 
        <StackLayout sdkExampleTitle sdkToggleNavButton style="background-color: white;"> 
-         <StackLayout class="m-x-10 m-t-15"> 
-            <Button class="text-primary text-left font-weight-bold" text="Cancel" (tap)="cancel()"></Button>
+         <StackLayout class="m-x-20 m-t-15" verticalALignment="center">  
+            <Gridlayout cols="auto,auto" verticalAlignment="center">
+            <StackLayout (tap)="cancel()" class="text-primary" row="0" col="0" orientation="horizontal">
+                <Label class="fa m-r-5" text="{{'fa-times' | fonticon}}"></Label>
+                <Label class="text-left font-weight-bold" text="Cancel"></Label>
+            </StackLayout>
+             <StackLayout (tap)="submit('Cancel')"  class="text-primary" row="0" col="0"  horizontalAlignment="right" orientation="horizontal">
+                <Label class="fa m-r-5" text="{{'fa-check' | fonticon}}"></Label>
+               <Label class="font-weight-bold" text="Submit"></Label> 
+             </StackLayout> 
+            </Gridlayout>  
          </StackLayout>
          <StackLayout class="hr-light m-15"></StackLayout>
-         <DatePicker id="datePicker"></DatePicker> 
-          <Button class="btn btn-primary btn-rounded-sm btn-active" text="Submit" (tap)="submit('Cancel')"></Button> 
+         <DatePicker id="datePicker"></DatePicker>  
        </StackLayout>
     `
 })
-export class ModalDatePicker implements OnInit{
+export class ModalDatePicker implements OnInit {
     @Input() public prompt: string;
-    constructor(private router: Router,private params: ModalDialogParams,private page: Page) {
+
+    constructor(private router: Router, private params: ModalDialogParams, private page: Page) {
         this.prompt = params.context.promptMsg;
     }
 
 
-    ngOnInit(){
-        let datePicker:DatePicker =<DatePicker> this.page.getViewById<DatePicker>("datePicker");
+    ngOnInit() {
+        let datePicker: DatePicker = <DatePicker> this.page.getViewById<DatePicker>("datePicker");
         let currentDate = new Date();
         datePicker.year = currentDate.getFullYear();
 
-        datePicker.month = currentDate.getMonth() +1;
+        datePicker.month = currentDate.getMonth() + 1;
         datePicker.day = currentDate.getDate();
         datePicker.minDate = new Date(1975, 0, 29);
         datePicker.maxDate = new Date(2045, 4, 12);
@@ -45,17 +54,16 @@ export class ModalDatePicker implements OnInit{
 
 
         /*let navigationExtras: NavigationExtras = {
-            queryParams: {
-                "date": '28/12/2016'
-            }
-        };*/
+         queryParams: {
+         "date": '28/12/2016'
+         }
+         };*/
 
 
-       // this.params.closeCallback(new Date(datePicker.year, datePicker.month, datePicker.day));
+        // this.params.closeCallback(new Date(datePicker.year, datePicker.month, datePicker.day));
         // this.router.navigate(["/calendar"], navigationExtras);
 
         this.params.closeCallback(datePicker.date);
-
 
 
         //let datePicker:DatePicker =<DatePicker> this.page.getViewById<DatePicker>("datePicker");
@@ -66,7 +74,6 @@ export class ModalDatePicker implements OnInit{
         this.params.closeCallback('');
 
     }
-
 
 
     ngOnDestroy() {
