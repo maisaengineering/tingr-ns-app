a55b8a3fa14425e48b338d44c8b5cbe67372269e
Upender Devulapally <upender.devulapally@gmail.com>
2017-01-24 21:36:42 +0530

    PT: #138152709 removed X btn from server error page

--- app/pages/dialogs/modal-server-error.ts
+++ app/pages/dialogs/modal-server-error.ts
@@ -11,22 +11,12 @@ let app = require("application");
 @Component({
     selector: 'modal-content',
     template: ` 
-       <StackLayout sdkExampleTitle sdkToggleNavButton style="background-color: white;" orientation="vertical"> 
-         <StackLayout class="m-x-20 m-t-15" verticalALignment="center">  
-            <Gridlayout cols="auto,auto" verticalAlignment="center"> 
-             <StackLayout (tap)="close('close')" class="text-primary blue2" row="0" col="0"  horizontalAlignment="right" orientation="horizontal">  
-               <Label text="&#xE5CD;" class="text-left material-icons md-36 md-close"></Label>
-             </StackLayout> 
-            </Gridlayout>  
-         </StackLayout>
-         <StackLayout class="hr-shadow m-5"></StackLayout>
-         <StackLayout orientation="vertical">
+       <StackLayout sdkExampleTitle sdkToggleNavButton style="background-color: white;" orientation="vertical" verticalAlignment="center">  
+         <StackLayout orientation="vertical" >
           <Image src="~/images/server-error.jpg" stretch="aspectFit"></Image>     
-         </StackLayout>
-         
+         </StackLayout> 
          <Button width="100" text="Ok"
-                    class="btn btn-primary blue2-background btn-rounded-sm" (tap)="close('close')"></Button>  
-          
+                    class="btn btn-primary blue2-background btn-rounded-sm" (tap)="close('close')"></Button>   
        </StackLayout>
     `
 })
