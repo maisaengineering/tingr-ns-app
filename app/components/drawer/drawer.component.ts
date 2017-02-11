406908c2a66875340bc78ffef24ffdb00eb5bd1f
Upender Devulapally <upender.devulapally@gmail.com>
2017-02-11 16:58:35 +0530

    teacher photo update

--- app/components/drawer/drawer.component.ts
+++ app/components/drawer/drawer.component.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
 import { TeacherInfo } from "../../providers/data/teacher_info";
-
+import {TeacherService} from "../../shared/teacher/teacher.service";
+let app = require("application");
 @Component({
     moduleId: module.id,
     selector: 'drawer-content',
     templateUrl: './drawer.component.html',
+    providers: [TeacherService]
 })
 export class DrawerComponent {
 
@@ -12,11 +14,42 @@ export class DrawerComponent {
     public teacherFullname: String;
     public teacherPhotograph: String;
     public teacherEmail: String;
+    public isAndroid: Boolean = false;
+    public isIos: Boolean = false;
+    constructor(private teacherService: TeacherService) {
+        if (app.android) {
+            this.isAndroid = true;
+        } else if (app.ios) {
+            this.isIos = true;
+        }
 
-    constructor() {
         this.teacherInfo = TeacherInfo.parsedDetails;
         this.teacherFullname = this.teacherInfo.fname+ ' '+this.teacherInfo.lname;
         this.teacherPhotograph = this.teacherInfo.photograph;
         this.teacherEmail = this.teacherInfo.email;
+        // update teacher profile in background
+        this.getProfileDetails();
     }
+
+    getProfileDetails(){
+        let teacher_klid = this.teacherInfo.teacher_klid;
+        this.teacherService.profileDetails(teacher_klid)
+            .subscribe(
+                (result) => {
+                    let body = result.body;
+                    this.teacherInfo.photograph = body.photograph;
+                    this.teacherInfo.fname = body.fname;
+                    this.teacherInfo.lname = body.lname;
+                    // update Teacher Info
+                    TeacherInfo.details = JSON.stringify(this.teacherInfo);
+                },
+                (error) => {
+
+                }
+            );
+    }
+
 }
+
+
+
--- app/pages/teacher/profile.component.ts
+++ app/pages/teacher/profile.component.ts
@@ -5,13 +5,19 @@ import {RouterExtensions} from 'nativescript-angular/router';
 import {TeacherService} from "../../shared/teacher/teacher.service";
 import {InternetService} from "../../shared/internet.service";
 import {ServerErrorService} from "../../shared/server.error.service";
-import { TeacherInfo } from "../../providers/data/teacher_info";
+import {TeacherInfo} from "../../providers/data/teacher_info";
+let platformModule = require("platform");
+let permissions = require("nativescript-permissions");
+let cameraModule = require("camera");
+import {GC} from 'utils/utils';
+let enums = require("ui/enums");
+let imagepicker = require("nativescript-imagepicker");
 import dialogs = require("ui/dialogs");
 let view = require("ui/core/view");
 let nstoasts = require("nativescript-toasts");
 let app = require("application");
 
-
+declare var android: any;
 @Component({
     moduleId: module.id,
     selector: 'my-app',
@@ -24,6 +30,8 @@ export class TeacherProfileComponent implements OnInit {
     public isAndroid: Boolean = false;
     public isIos: Boolean = false;
     public teacherInfo: any;
+    public selectedImages = [];
+    public picUploaded: Boolean = false;
 
     constructor(private page: Page, private location: Location,
                 private routerExtensions: RouterExtensions,
@@ -42,12 +50,139 @@ export class TeacherProfileComponent implements OnInit {
 
     ngOnInit() {
         // show alert if no internet connection
-        console.log("Details "+ JSON.stringify(this.teacherInfo))
         this.internetService.alertIfOffline();
+
     }
 
+
+
     goBack() {
-        this.routerExtensions.backToPreviousPage();
+        this.routerExtensions.navigate(["/calendar"], {
+            transition: {
+                name: "slideRight"
+            },
+        });
     }
+
+    selectMomentCaptureOption() {
+        dialogs.action({
+            //message: "",
+            cancelButtonText: "Cancel",
+            actions: ["Take photo", "Choose existing"]
+        }).then(result => {
+            if (result === 'Take photo') {
+                //  Android permissions (mainly for API 23+/Android 6+) check inplace
+                // This wraps up the entire Android 6 permissions system into a nice easy to use promise.
+                // In addition, you can also have multiple permissions pending and each one will resolve properly
+                if (platformModule.device.os === "Android" && platformModule.device.sdkVersion >= 23) {
+                    permissions.requestPermission(android.Manifest.permission.CAMERA, "Allow Tingr to access your camera?")
+                        .then(() => {
+                            //console.log("CAMERA Permission: granted!");
+                            this.takePicture();
+                        })
+                        .catch(() => {
+                            //console.log("CAMERA Permission: -- refused");
+                        });
+                } else {
+                    this.takePicture();
+                }
+
+            } else if (result === 'Choose existing') {
+                if (platformModule.device.os === "Android" && platformModule.device.sdkVersion >= 23) {
+                    permissions.requestPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, "Allow Tingr to access your gallery?")
+                        .then(() => {
+                            //console.log("READ_EXTERNAL_STORAGE permission: granted!");
+                            this.selectFromGallery();
+                        })
+                        .catch(() => {
+                            //console.log("READ_EXTERNAL_STORAGE permission: -- refused");
+                        });
+                } else {
+                    this.selectFromGallery();
+                }
+            }
+        });
+    }
+
+
+    takePicture() {
+        let teacherProfilePicView = view.getViewById(this.page, 'teacherProfilePic');
+        let options = {
+            saveToGallery: this.isAndroid ? false : true
+        };
+        cameraModule.takePicture(options).then((imageAsset) => {
+
+            let imageBase64Data =  imageAsset.toBase64String(enums.ImageFormat.jpeg);
+            this.picUploaded = true;
+            teacherProfilePicView.src = imageAsset;
+            this.changePhoto(imageBase64Data);
+            GC();
+        });
+    }
+
+    selectFromGallery() {
+        let context = imagepicker.create({
+            mode: "single"
+        });
+        this.startImageSelection(context);
+    }
+
+    startImageSelection(context) {
+        let teacherProfilePicView = view.getViewById(this.page, 'teacherProfilePic');
+
+        context
+            .authorize()
+            .then(() => {
+                return context.present();
+            })
+            .then((selection) => {
+                //console.log("Selection done:");
+                selection.forEach((selected) => {
+                    //TODO for multiple seelction follow below coding for each one
+                    // console.log("----------------");
+                    // console.log("uri: " + selected.uri);
+                    // console.log("fileUri: " + selected.fileUri);
+                });
+                this.selectedImages = selection;
+                // this.changeDetectionRef.detectChanges();
+                let selectedImage = this.selectedImages[0];
+                selectedImage
+                    .getImage()
+                    .then((imageSource) => {
+
+                        let imageBase64Data = imageSource.toBase64String(enums.ImageFormat.jpeg);
+                        this.picUploaded = true;
+                        teacherProfilePicView.src = imageSource;
+                        this.changePhoto(imageBase64Data);
+
+                    }).catch((e) => {
+                    //console.log("Error: " + e);
+                    //console.log(e.stack);
+                });
+                GC();
+            }).catch((e) => {
+            //console.log(e);
+        });
+
+    }
+
+    changePhoto(imageBase64Data){
+       let teacher_klid = this.teacherInfo.teacher_klid;
+        this.teacherService.changePhotoGraph(imageBase64Data,teacher_klid)
+            .subscribe(
+                (result) => {
+                   let body = result.body;
+                    console.log("result "+ JSON.stringify(body));
+                    this.teacherInfo.photograph = body.photograph;
+                    //update teacher info in app settings
+                    TeacherInfo.details = JSON.stringify(this.teacherInfo);
+                },
+                (error) => {
+                    console.log("error "+ JSON.stringify(error));
+                    this.serverErrorService.showErrorModal();
+                }
+            );
+    }
+
 
 }
