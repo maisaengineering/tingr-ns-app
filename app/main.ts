a987b88dba7f454c169ef831240948df449c9f35
imaginationcoder <labs@maisasolutions.com>
2017-03-14 14:54:30 +0530

    firebase plugin integration

--- app/main.ts
+++ app/main.ts
@@ -14,5 +14,21 @@ enableProdMode();
 
 //StatusBar.setColor('#ffffff');
 
+import firebase = require("nativescript-plugin-firebase");
+firebase.init({
+    // Optionally pass in properties for database, authentication and cloud messaging,
+    // see their respective docs.
+}).then(
+    (instance) => {
+        console.log("-----------------  firebase.init done");
+    },
+    (error) => {
+        console.log("-----------------  firebase.init error: " + error);
+    }
+);
+
+
+
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
 
